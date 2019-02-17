e5b88416590e6607e60b4248bf2a8cdd9263557b
kaisukez <zykigix@hotmail.com>
2019-02-17 11:44:40 +0700

    Updates

---
+++ precache-manifest.0b921480e7ba0615ec267809952d3702.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "9a83b77bb768547fa754",
+    "url": "/ywc-interview/static/css/main.40d35ac2.chunk.css"
+  },
+  {
+    "revision": "9a83b77bb768547fa754",
+    "url": "/ywc-interview/static/js/main.9a83b77b.chunk.js"
+  },
+  {
+    "revision": "dc8ed9c1a5f7294bf9b5",
+    "url": "/ywc-interview/static/js/runtime~main.dc8ed9c1.js"
+  },
+  {
+    "revision": "a82e601e4a3e423be35a",
+    "url": "/ywc-interview/static/js/2.a82e601e.chunk.js"
+  },
+  {
+    "revision": "59002b530837b7efd5e5fbdcfc549b83",
+    "url": "/ywc-interview/static/media/doge.59002b53.jpg"
+  },
+  {
+    "revision": "c80106af4b75f56e340cf2d373d113a7",
+    "url": "/ywc-interview/static/media/background-new.c80106af.jpg"
+  },
+  {
+    "revision": "2874206b21420d7f8301565ec59b62d3",
+    "url": "/ywc-interview/index.html"
+  }
+];
